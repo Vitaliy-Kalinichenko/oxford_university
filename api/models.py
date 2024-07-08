4df3fe6e15cb4b8ec95481ceab349522eb734a57
vkalinichenko <vkalinichenko@perfsys.com>
2024-07-08 18:05:55 +0300

    crud user

---
+++ api/models.py
@@ -0,0 +1,82 @@
+import re
+from http import HTTPStatus
+from typing import Optional
+from uuid import UUID
+
+from fastapi import HTTPException
+from pydantic import BaseModel, EmailStr, validator, constr
+
+#########################
+# BLOCK WITH API MODELS #
+#########################
+
+
+LETTER_MATCH_PATTERN = re.compile(r"^[а-яА-Яa-zA-Z\-]+$")
+
+
+class TunedModel(BaseModel):
+    class Config:
+        """tells pydantic to convert even non dict obj to json"""
+
+        orm_mode = True
+
+
+class CreateUserResponse(TunedModel):
+    user_id: UUID
+    name: str
+    surname: str
+    email: EmailStr
+    is_active: bool
+
+
+class DeleteUserResponse(BaseModel):
+    deleted_user_id: UUID
+
+
+class UpdateUserResponse(BaseModel):
+    updated_user_id: UUID
+
+
+class GetUserResponse(TunedModel):
+    user_id: UUID
+    name: str
+    surname: str
+    email: EmailStr
+    is_active: bool
+
+
+class BaseUser(BaseModel):
+    name: Optional[constr(min_length=1)]
+    surname: Optional[constr(min_length=1)]
+    email: Optional[EmailStr]
+
+    @validator("name")
+    def validate_name(cls, value):
+        if not LETTER_MATCH_PATTERN.match(value):
+            raise HTTPException(
+                status_code=HTTPStatus.UNPROCESSABLE_ENTITY, detail="Name should contains only letters."
+            )
+        return value
+
+    @validator("surname")
+    def validate_surname(cls, value):
+        if not LETTER_MATCH_PATTERN.match(value):
+            raise HTTPException(
+                status_code=HTTPStatus.UNPROCESSABLE_ENTITY, detail="Surname should contains only letters."
+            )
+        return value
+
+    class Config:
+        extra = "forbid"
+
+
+class CreateUser(BaseUser):
+    name: constr(min_length=1)
+    surname: constr(min_length=1)
+    email: EmailStr
+
+
+class UpdateUser(BaseUser):
+    pass
+
+
---
+++ main.py
@@ -0,0 +1,22 @@
+import uvicorn
+from fastapi import FastAPI, APIRouter
+
+from api.handlers import user_router
+
+#########################
+# BLOCK WITH API ROUTES #
+#########################
+
+# create instance of the app
+app = FastAPI(title="oxford-university")
+
+# create the instance for the routes
+main_api_router = APIRouter()
+
+# set routes to the app instance
+main_api_router.include_router(user_router, prefix="/user", tags=["user"])
+app.include_router(main_api_router)
+
+if __name__ == "__main__":
+    # run app on the host and port
+    uvicorn.run(app, host="0.0.0.0", port=8000)
